f41cd3a04d72bf5276c4f3a31861341bd3a9f8e1
Sanskriti <sanskritikhanal5@gmail.com>
2024-12-31 15:48:27 +0545

    Make Responsive

---
+++ Desktop/GuffGuthi - Copy 2/README.md
@@ -0,0 +1,3 @@
+# GuffGuthi - Social news platform
+
+GuffGuthi aims to offer an interactive online platform that blends real-time communication with community-orientated support for significant concerns, creating more profound connections and a positive social impact.
---
+++ Desktop/GuffGuthi - Copy 2/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword"
+import Profile from "./components/Profile";
+import EditProfile from "./components/EditProfile";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+   
+  );
+}
+
+export default App;
